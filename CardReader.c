d2db89e91c0f9aa0faa0f7c88441476c409ee89b
LukeMarlin <luke.marlin@viacesi.fr>
2013-11-22 17:05:38 +0100

    File separation and improvements (unusable version)

--- CardReader.c
+++ CardReader.c
@@ -1,8 +1,10 @@
-typedef struct{
+typedef struct CardReader{
 
 char* name;
+char* tag;
 int GPIO_0;
 int GPIO_1;
+int bitCount;
 
 }CardReader;
 
--- main.c
+++ main.c
@@ -1,56 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #include <wiringPi.h>
 #include "CardReader.c"
+#include "main.h"
+#include "pins.c"
 
 // Which GPIO pin we're using
-#define PIN_0 0
-#define PIN_1 1
-#define PIN_4 4
-#define PIN_7 7
-#define PIN_8 8
-#define PIN_9 9
-#define PIN_10 10
-#define PIN_11 11
-#define PIN_14 14
-#define PIN_15 15
-#define PIN_17 17
-#define PIN_18 18
-#define PIN_21 21
-#define PIN_22 22
-#define PIN_23 23
-#define PIN_24 24
-#define PIN_25 25
 #define UNUSED_PIN -1
 #define READERS_COUNT 8
+#define FRAME_SIZE 26
 
 int pins[17] = {PIN_0, PIN_1, PIN_4, PIN_7, PIN_8, PIN_9, PIN_10, PIN_11, PIN_14, PIN_15, PIN_17, PIN_18, PIN_21, PIN_22, PIN_23, PIN_24, PIN_25};
 int values[17] = {};
 CardReader** readers;
 
-// Handler for interrupt
-void handle(int GPIO) {
-	printf("ID : %ld", (long int)syscall(224));
-	printf("%d\n", GPIO);
-	fflush(stdout);
-}
+#include "callbacks.c"
 
 void initProgram(){
 	wiringPiSetupGpio();
-	readers = (CardReader**)malloc(sizeof(CardReader*)*READERS_COUNT);
-	CardReader test;
-	test.GPIO_0 = 1;
-	readers[0]=&test;
-	printf("Salut %d", readers[0]->GPIO_0);
-
+	readers = (CardReader**)malloc(sizeof(CardReader*)*READERS_COUNT);	
+	createCardReader("reader1", PIN_23, PIN_24);
+	printf("[%d]", readers[PIN_23]->name);
 }
 
 int main(void) {
 	
 	initProgram();
 	printf("ID : %ld\n", (long int)syscall(224));
-	printf("%d\n", pins[5]);
-	printf("%d\n", values[7]);
+	printf("%d\n", pins[PIN_23]);
+	printf("%d\n", values[PIN_24]);
+	
 	// Set pin to output in case it's not
 	pinMode(PIN_23, INPUT);
 	pinMode(PIN_24, INPUT);
@@ -58,8 +38,8 @@ int main(void) {
 	pullUpDnControl(PIN_24, PUD_UP);
 	
 	// Bind to interrupt
-	wiringPiISR(PIN_23, INT_EDGE_FALLING, &handle);
-	wiringPiISR(PIN_24, INT_EDGE_FALLING, &handle);
+	wiringPiISR(PIN_23, INT_EDGE_FALLING, &callback23);
+	wiringPiISR(PIN_24, INT_EDGE_FALLING, &callback24);
 
 		
 
@@ -69,3 +49,20 @@ int main(void) {
 	}
 }
 
+CardReader createCardReader(char* pname,int pGPIO_0, int pGPIO_1){
+	CardReader temp;
+	
+	temp.name = pname;
+	temp.GPIO_0 = pGPIO_0;
+	temp.GPIO_1 = pGPIO_1;
+	temp.tag = malloc(sizeof(char*)*FRAME_SIZE);
+	temp.bitCount = 0;
+
+	readers[pGPIO_0] = &temp;
+	readers[pGPIO_1] = &temp;
+	values[pGPIO_0] = 0;
+	values[pGPIO_1] = 1;
+
+	return temp;
+
+}
