60710b57b373d7ac54c7ad82d5434299b6470e37
Luke Marlin <luke.marlin@viacesi.fr>
2017-06-30 23:28:38 +0000

    Removed .php API calls, now directly working with DB

--- python-scripts/getTags.py
+++ python-scripts/getTags.py
@@ -1,117 +1,54 @@
+#!/usr/bin/python3.2
+import pymssql
 from datetime import datetime
-import http.client
-import json
-import hashlib
 import argparse
 import sys
+import os
 
-#ServerAddress = "10.154.100.101"
-ServerAddress = "172.17.100.1"
-TagsVersionNumberPage="/fred/dev/api.php?p=badgeversion"
-ChallengeWordPage = "/fred/dev/api.php?p=authAsk"
-GetListPage = "/fred/dev/api.php?p=getAuthorizedTags&key="
-DBTagsVersionNumber = 0;
-userJSONid = "1"
-clubJSONid = "2"
-adminJSONid = "3"
 userTagsFile = "userTags.txt"
-clubTagsFile = "clubTags.txt"
+userPlusTagsFile = "userPlusTags.txt"
 adminTagsFile = "adminTags.txt"
-Password = ""
-Timeout = 10
-Port = 80
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--tagBaseVersion', help='Current tags base version. Used to avoid unnecessary loadings. Default=0')
 args = parser.parse_args()
 baseVersion = args.tagBaseVersion if args.tagBaseVersion else 0
+login = "";
+pwd = "";
 
-#Connection to server
-conn = http.client.HTTPConnection(ServerAddress, Port, Timeout)
-
-#First step, we retrieve the current version number on DB
-conn.request("GET", TagsVersionNumberPage)
-r0 = conn.getresponse();
-if r0.status is 200:
-	DBTagsVersionNumber = r0.read().decode("utf-8")
-else:
-	#print("Failed to retrieve version number")
-	sys.exit(0)
-if baseVersion == 0 or baseVersion != DBTagsVersionNumber:
-
-	def saltAndHash(word):
-		h = hashlib.sha256()
-		temp = Password + word + Password
-		h.update(temp.encode('utf-8'))
-		return h.hexdigest()
-
-	conn.request("GET", ChallengeWordPage)
-	r1 = conn.getresponse()
-
-	#If return code is 200, the page was correctly loaded
-	if r1.status is 200:
-		data1 = r1.read()  # This will return entire content.
-
-		try:
-			table = json.loads(data1.decode("utf-8")) #Convert data in string and decode JSON
-		except ValueError:
-			#print("Failed to decode json: " + data1)
-			sys.exit(4)
-		key = saltAndHash(table['mot'])
+try:
+    login = os.environ['tcpdblogin']
+    password = os.environ['tcpdbpassword']
+except:
+    sys.exit(3)
 
 
-		#Second request with key to get the RFID tag list
-		conn.request("GET", GetListPage + key)
-		r2 = conn.getresponse()
+try:
+    with pymssql.connect(server="213.246.49.130", user=login, password=password, database="tcp_db") as conn:
+        cur = conn.cursor()
+        cur.execute("select versionNumber from AuthorizedTagsVersion")
+        new_version = cur.next()[0]
 
-		#Return code 200 = OK
-		if r2.status is 200:
-			data2 = r2.read()
-			try:
-				RFIDtable = json.loads(data2.decode("utf-8")) #Convert data in string and decode JSON
-			except ValueError:
-				#print("Failed to decode json: " + data1)
-				sys.exit(3)
+        if baseVersion == new_version:
+            sys.exit(0)
 
-			#Write each user tag in file
-			try:
-				f = open(userTagsFile,'w')
-			except ValueError:
-				sys.exit(5)
+        cur.execute("""select b.number, c.Category_ID from BadgeJeu as b
+                        inner join PlayerJeu as p on b.Player_ID = p.ID
+                        inner join PlayerCategory as c on c.Player_ID = p.ID
+                        where b.isEnabled = 1""")
+        try:
+            with open(userTagsFile, 'w') as userFile, open(userPlusTagsFile, 'w') as userPlusFile, open(adminTagsFile, 'w') as adminFile:
 
-			for tag in RFIDtable[userJSONid]:
-				f.write(str(tag) + '\n')
-			f.close()
+                for row in cur:
+                    if row[1] == 1:
+                        userFile.write(str(row[0]) + '\n')
+                    if row[1] == 2:
+                        userPlusFile.write(str(row[0]) + '\n')
+                    if row[1] == 3:
+                        adminFile.write(str(row[0]) + '\n')
+        except:
+            sys.exit(2)
+except:
+    sys.exit(1)
 
-			#Write each club tag in file
-			try:
-				f = open(clubTagsFile,'w')
-			except ValueError:
-				sys.exit(5)
-
-			for tag in RFIDtable[clubJSONid]:
-				f.write(str(tag) + '\n')
-			f.close()
-
-			#Write each admin tag in file
-			try:
-				f = open(adminTagsFile,'w')
-			except ValueError:
-				sys.exit(5)
-
-			for tag in RFIDtable[adminJSONid]:
-				f.write(str(tag) + '\n')
-			f.close()
-
-
-		else:
-			sys.exit(1)
-			#print("Failed to get the RFID tag list")
-
-	else:
-		sys.exit(2)
-		#print("Failed to get the challenge word")
-
-
-conn.close()
 sys.exit(0)
